451ce738fd6e1674a3840cfb8dbb8b321c200089
Meindert <mkroese@users.noreply.github.com>
2026-03-31 21:25:13 +0200

    make more than one refresh possible

--- src/refresh.py
+++ src/refresh.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
+from collections import ChainMap
 from flask import Flask,render_template,request,abort
+from os import getenv
 import yaml
 
 app = Flask(__name__)
 config = {}
-path = ''
+paths = {}
 
 @app.route(f'/<path>')
 def form(path):
-    assert path==config['path']
-    if path==config['path']:
-        return render_template('form.html')
+    if path in paths:
+        return render_template('form.html',url=request.base_url)
     else:
         return abort(404)
 
 @app.route('/refresh', methods=['POST'])
 def refresh():
+    url = request.form['url']
     token = request.form['token']
-    if token==config['token']:
-        open(config['refresh'],'w')
+    path = url.split('/')[-1]
+    if token == config[paths[path]]['token']:
+        open(config[paths[path]]['refresh'],'w')
         return render_template('done.html',
                                 message=config['message'],
                                 url=config['link'])
@@ -26,10 +29,11 @@ def refresh():
         return abort(404)
 
 if __name__ == '__main__':
-    refresh_path = get_env('REFRESH_PATH')
+    refresh_path = getenv('REFRESH_PATH')
     with open(f'{refresh_path}/refresh.yaml', 'r') as f:
-        config = yaml.load(f, Loader=yaml.SafeLoader)['vocab']
-        print(config)
-        path = config['path']
+        ext_config = yaml.load(f, Loader=yaml.SafeLoader) #['vocab']
+        for key in ext_config.keys():
+            config[key] = dict(ChainMap(*ext_config[key]))
+            paths[config[key]['path']] = key
     app.run(host='0.0.0.0', port=80)
 
