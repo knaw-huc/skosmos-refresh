a9f783ff77be00ef4ef83d64c71ef3b2ad155968
Menzo Windhouwer <menzo.windhouwer@di.huc.knaw.nl>
2026-05-25 18:46:04 +0200

    fix

--- src/refresh.py
+++ src/refresh.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-from collections import ChainMap
 from flask import Flask,render_template,request,abort
-from os import getenv
 import yaml
+from os import getenv
 
 app = Flask(__name__)
 config = {}
-paths = {}
 
 @app.route(f'/<path>')
 def form(path):
-    if path in paths:
+    if path in config:
         return render_template('form.html')
     else:
         return abort(404)
@@ -19,20 +17,18 @@ def form(path):
 def refresh():
     token = request.form['token']
     path = request.headers['Referer'].split('/')[-1]
-    if token == config[paths[path]]['token']:
-        open(config[paths[path]]['refresh'],'w')
+    if path in config and token==config[path]['token']:
+        open(config[path]['refresh'],'w')
         return render_template('done.html',
-                                message=config[paths[path]]['message'],
-                                url=config[paths[path]]['link'])
+                                message=config[path]['message'],
+                                url=config[path]['link'])
     else:
         return abort(404)
 
 if __name__ == '__main__':
     refresh_path = getenv('REFRESH_PATH')
     with open(f'{refresh_path}/refresh.yaml', 'r') as f:
-        ext_config = yaml.load(f, Loader=yaml.SafeLoader) #['vocab']
-        for key in ext_config.keys():
-            config[key] = dict(ChainMap(*ext_config[key]))
-            paths[config[key]['path']] = key
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+        print(config)
     app.run(host='0.0.0.0', port=80)
 
